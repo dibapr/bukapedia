5b54c8a761b7c25abd7fdbbe9c97d8e20e1d2b2a
Dimas Bayu Pratama <dimasbayu6401@gmail.com>
2023-05-18 06:20:56 +0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from "react";
 import { Navbar } from "./components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product/Index";
+import Cart from "./pages/Cart/Index";
+import Login from "./pages/Login/Index";
+import Admin from "./pages/Admin/Index";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route index path="/" element={<Product />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="login" element={<Login />} />
+            <Route path="admin" element={<Admin />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
