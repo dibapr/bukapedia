77abd149576e3ec54eddff7d4b9db1ef50638cf6
Dimas Bayu Pratama <dimasbayu6401@gmail.com>
2023-05-18 23:59:30 +0700

    change pages subfolder to be self-explanatory

---
+++ src/pages/LoginPage/Index.jsx
@@ -0,0 +1,12 @@
+import LoginContainer from "../../components/LoginContainer/LoginContainer";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-bold">Login</h1>
+      <LoginContainer />
+    </div>
+  );
+};
+
+export default LoginPage;
